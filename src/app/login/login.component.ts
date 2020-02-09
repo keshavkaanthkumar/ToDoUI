e2bb72ecc930f5dadd84ebf31d63eba75dab6795
Keshav Kaanth Kumar <kumar.ke@husky.neu.edu>
2020-02-09 14:13:25 -0500

    onboarding

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { AuthenticationService, TokenPayload } from '../authentication.service';
+import { Router } from '@angular/router';
+import {MatSpinner} from '@angular/material'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  credentials: TokenPayload = {
+    email: '',
+    password: ''
+  };
+  loginStatus: any;
+  loginFailure: boolean = false;
+  constructor(private auth: AuthenticationService, private router: Router) {}
+
+  login() {
+    this.auth.login(this.credentials).subscribe(() => {
+      this.router.navigateByUrl('/profile');
+    }, 
+      (err) => {
+        this.loginFailure = true;
+        this.loginStatus = err.error.message;
+        console.error(err);
+      });
+    
+  }
+}
